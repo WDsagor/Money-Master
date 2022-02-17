7571ab497b69b15519cace0b393e29b897ffb810
WDsagor <sagarwpress@gmail.com>
2022-02-17 20:25:25 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,69 @@
+
+document.getElementById('costCal').addEventListener("click", function(){
+    const incomeAmout = document.getElementById('incomeValue');
+    const totalIncomeAmout = parseInt(incomeAmout.value);
+
+
+    // const totalIncomeAmout = convertValue('incomeValue');
+    const toatalFoodCost = convertValue('foodCost');
+    const totalRentCost = convertValue('rentCost');
+    const totalClothCost = convertValue('clothCost');
+
+    //    const totalExenses = toatalFoodCost + totalRentCost + totalClothCost;
+
+    
+    if(toatalFoodCost>=0 && totalRentCost>= 0 && totalClothCost>=0){
+        const totalExenses = toatalFoodCost + totalRentCost + totalClothCost;
+        const balance = totalIncomeAmout - totalExenses;
+        const expenses = document.getElementById('totalExenses');
+        expenses.innerText = totalExenses;
+        if(totalIncomeAmout >= totalExenses){
+            document.getElementById('balance').innerText = balance;
+            document.getElementById('remainBalance').innerText = balance;
+        }
+        else{
+            alert("You haven't enough Income balance")
+        }
+    }
+    else{
+        alert("Please input possetive value")
+    }
+});
+
+document.getElementById("saveBtn").addEventListener('click', function(){
+    const incomeAmout = document.getElementById('incomeValue');
+    const totalIncomeAmout = parseInt(incomeAmout.value);
+    const saveParsent = convertValue('saveParsent');
+    const saveAmount = totalIncomeAmout*(saveParsent/100);
+    if(saveAmount>0 ){
+        const balanceAmount = document.getElementById('balance').innerText
+        const convertAmount = parseInt(balanceAmount)
+        if(balanceAmount>=saveAmount){
+            document.getElementById('saveAmount').innerText = saveAmount;
+            document.getElementById('remainBalance').innerText = convertAmount-saveAmount;
+        }
+        else{
+            alert("You haven't enough balance")
+        }
+    }
+    else{
+        alert("Please input possetive value in Save")
+    }
+    
+})
+
+
+
+function convertValue (inputId){
+    const selectId = document.getElementById(inputId);
+    const convert = parseInt(selectId.value);
+    selectId.value = "";
+    return convert;
+     
+};
+
+
+// const incomeAmout = document.getElementById('incomeValue');
+// const foodCost = document.getElementById('foodCost');
+// const rentCost = document.getElementById('rentCost');
+// const clothCost = document.getElementById('clothCost');
